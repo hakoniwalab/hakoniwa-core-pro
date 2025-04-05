eafdc150319cf8703581dbfafe02f7c4dc705527
Takashi Mori <kanetugu2015@gmail.com>
2025-04-05 09:15:43 +0900

    add recv event

--- sources/core/include/hako_pro_data.hpp
+++ sources/core/include/hako_pro_data.hpp
@@ -1,9 +1,13 @@
 #pragma once
+#ifndef HAKO_CORE_EXTENSION
+#define HAKO_CORE_EXTENSION
+#endif
 
 #include <hako_extension.hpp>
 #include "utils/hako_share/hako_shared_memory_factory.hpp"
+#include "data/hako_master_data.hpp"
+#include "core/context/hako_context.hpp"
 #include <memory>
-
 namespace hako::data::pro {
 
 typedef enum {
@@ -14,9 +18,9 @@ typedef enum {
 #define HAKO_ASSET_ID_EXTERNAL -1
 
 typedef struct {
+    bool enabled;
     bool recv_flag;
-    int proc_id;
-    int asset_id;
+    pid_type proc_id;
     int real_channel_id;
     HakoRecvEventType type;
     void (*on_recv)();
@@ -28,12 +32,15 @@ typedef struct {
     int entry_num;
 } HakoRecvEventTableType;
 
-class HakoProMasterExtension;
 class HakoProAssetExtension;
-class HakoProData : public std::enable_shared_from_this<HakoProData> {
+class HakoProData : public std::enable_shared_from_this<HakoProData>, public hako::extension::IHakoMasterExtension {
     public:
-        HakoProData()
+        HakoProData(std::shared_ptr<data::HakoMasterData> master_data)
         {
+            this->master_data_ = master_data;
+            this->shmp_ = nullptr;
+            this->recv_event_table_ = nullptr;
+            this->shm_type_ = "shm";
         }
         void init(const std::string& type="shm")
         {
@@ -42,8 +49,15 @@ class HakoProData : public std::enable_shared_from_this<HakoProData> {
             if (this->shmp_ == nullptr) {
                 throw std::runtime_error("Failed to create shared memory");
             }
-            master_extension_ = std::make_shared<HakoProMasterExtension>(shared_from_this());
             asset_extension_ = std::make_shared<HakoProAssetExtension>(shared_from_this());
+        }
+        void destroy()
+        {
+            if (this->shmp_ != nullptr) {
+                this->shmp_->destroy_memory(HAKO_SHARED_MEMORY_ID_1);
+                this->shmp_ = nullptr;
+                this->recv_event_table_ = nullptr;
+            }
         }
         virtual ~HakoProData()
         {
@@ -56,43 +70,128 @@ class HakoProData : public std::enable_shared_from_this<HakoProData> {
         {
             recv_event_table_ = table;
         }
-        private:
+        HakoRecvEventTableType* get_recv_event_table()
+        {
+            return recv_event_table_;
+        }
+        virtual bool on_pdu_data_create() override;
+        virtual bool on_pdu_data_load() override;
+        virtual bool on_pdu_data_reset() override;
+        virtual bool on_pdu_data_destroy() override;
+    
+        bool register_data_recv_event(const std::string& robot_name, int channel_id, void (*on_recv)())
+        {
+            if (recv_event_table_ == nullptr) {
+                return false;
+            }
+            HakoPduChannelIdType real_id = this->master_data_->get_pdu_data()->get_pdu_channel(robot_name, channel_id);
+            if (real_id < 0) {
+                return false;
+            }
+        
+            if (recv_event_table_->entry_num >= HAKO_RECV_EVENT_MAX) {
+                return false;
+            }
+            this->shmp_->lock_memory(HAKO_SHARED_MEMORY_ID_2);
+            for (int i = 0; i < recv_event_table_->entry_num; i++) {
+                if (recv_event_table_->entries[i].enabled == false) {
+                    hako::core::context::HakoContext context;
+                    recv_event_table_->entries[i].enabled = true;
+                    recv_event_table_->entries[i].recv_flag = false;
+                    recv_event_table_->entries[i].proc_id = context.get_pid();
+                    recv_event_table_->entries[i].real_channel_id = real_id;
+                    if (on_recv != nullptr) {
+                        recv_event_table_->entries[i].type = HAKO_RECV_EVENT_TYPE_CALLBACK;
+                        recv_event_table_->entries[i].on_recv = on_recv;
+                    }
+                    else {
+                        recv_event_table_->entries[i].type = HAKO_RECV_EVENT_TYPE_FLAG;
+                        recv_event_table_->entries[i].on_recv = nullptr;
+                    }
+                    break;
+                }
+            }
+            recv_event_table_->entry_num++;
+            this->shmp_->unlock_memory(HAKO_SHARED_MEMORY_ID_2);
+            return true;
+        }
+        /*
+         * if asset_id < 0 then it is external 
+         * channel_id must be real channel id!
+         */
+        bool get_recv_event(int asset_id, int channel_id, int& recv_event_id)
+        {
+            recv_event_id = -1;
+            if (recv_event_table_ == nullptr) {
+                return false;
+            }
+            if (channel_id >= HAKO_PDU_CHANNEL_MAX) {
+                return false;
+            }
+            if (recv_event_table_->entry_num == 0) {
+                return false;
+            }
+            hako::core::context::HakoContext context;
+            auto pid = context.get_pid();
+            for (int i = 0; i < recv_event_table_->entry_num; i++) {
+                if (recv_event_table_->entries[i].enabled == false) {
+                    continue;
+                }
+                if (recv_event_table_->entries[i].proc_id != pid) {
+                    continue;
+                }
+                if (recv_event_table_->entries[i].real_channel_id != channel_id) {
+                    continue;
+                }
+                this->master_data_->get_pdu_data()->read_pdu_spin_lock(asset_id, channel_id);
+                bool recv_flag = recv_event_table_->entries[i].recv_flag;
+                if (recv_flag) {
+                    recv_event_table_->entries[i].recv_flag = false;
+                }
+                this->master_data_->get_pdu_data()->read_pdu_spin_unlock(asset_id, channel_id);
+                recv_event_id = i;
+                return true;
+            }
+            return false;
+        }
+        bool call_recv_event_callback(int recv_event_id)
+        {
+            if (recv_event_table_ == nullptr) {
+                return false;
+            }
+            if (recv_event_id < 0 || recv_event_id >= HAKO_RECV_EVENT_MAX) {
+                return false;
+            }
+            if (recv_event_table_->entries[recv_event_id].enabled == false) {
+                return false;
+            }
+            if (recv_event_table_->entries[recv_event_id].type != HAKO_RECV_EVENT_TYPE_CALLBACK) {
+                return false;
+            }
+            if (recv_event_table_->entries[recv_event_id].on_recv == nullptr) {
+                return false;
+            }
+            recv_event_table_->entries[recv_event_id].on_recv();
+            return true;
+        }
+    private:
         HakoRecvEventTableType* recv_event_table_;
-        std::shared_ptr<HakoProMasterExtension> master_extension_;
         std::shared_ptr<HakoProAssetExtension> asset_extension_;
         std::shared_ptr<hako::utils::HakoSharedMemory> shmp_;
         std::string shm_type_;
-    };
-    
-class HakoProMasterExtension: public hako::extension::IHakoMasterExtension {
-public:
-    HakoProMasterExtension(std::shared_ptr<HakoProData> ptr)
-    {
-        pro_ = ptr;
-    }
-    virtual ~HakoProMasterExtension()
-    {
-    }
-    virtual bool on_pdu_data_create() override;
-    virtual bool on_pdu_data_load() override;
-    virtual bool on_pdu_data_reset() override;
-    virtual bool on_pdu_data_destroy() override;
-    
-private:
-    std::shared_ptr<HakoProData> pro_;
+        std::shared_ptr<data::HakoMasterData> master_data_;
 };
+    
 class HakoProAssetExtension: public hako::extension::IHakoAssetExtension {
 public:
     HakoProAssetExtension(std::shared_ptr<HakoProData> ptr)
     {
         pro_ = ptr;
-
     }
     virtual ~HakoProAssetExtension()
     {
     }
-    virtual bool on_pdu_data_write(int asset_id, int real_channel_id) override;
-    virtual bool on_pdu_data_write_for_external(int real_channel_id) override;
+    virtual bool on_pdu_data_write(int real_channel_id) override;
 private:
     std::shared_ptr<HakoProData> pro_;
 };
--- sources/core/src/hako_pro_data.cpp
+++ sources/core/src/hako_pro_data.cpp
@@ -2,27 +2,60 @@
 
 using namespace hako::data;
 
-bool pro::HakoProMasterExtension::on_pdu_data_create()
+bool pro::HakoProData::on_pdu_data_create()
 {
-    auto shmid = this->pro_->get_shared_memory()->create_memory(HAKO_SHARED_MEMORY_ID_2, sizeof(HakoRecvEventTableType));
+    auto shmid = this->get_shared_memory()->create_memory(HAKO_SHARED_MEMORY_ID_2, sizeof(HakoRecvEventTableType));
     if (shmid < 0) {
         return false;
     }
     (void)shmid;
-    void *datap = this->pro_->get_shared_memory()->lock_memory(HAKO_SHARED_MEMORY_ID_2);
-    this->pro_->set_recv_event_table(static_cast<HakoRecvEventTableType*>(datap));
+    void *datap = this->get_shared_memory()->lock_memory(HAKO_SHARED_MEMORY_ID_2);
+    this->set_recv_event_table(static_cast<HakoRecvEventTableType*>(datap));
     {
         memset(datap, 0, sizeof(HakoRecvEventTableType));
     }
-    this->pro_->get_shared_memory()->unlock_memory(HAKO_SHARED_MEMORY_ID_2);
+    this->get_shared_memory()->unlock_memory(HAKO_SHARED_MEMORY_ID_2);
     return true;
 }
-bool pro::HakoProMasterExtension::on_pdu_data_load()
+bool pro::HakoProData::on_pdu_data_load()
 {
-    void *datap = this->pro_->get_shared_memory()->load_memory(HAKO_SHARED_MEMORY_ID_2, sizeof(HakoRecvEventTableType));
+    void *datap = this->get_shared_memory()->load_memory(HAKO_SHARED_MEMORY_ID_2, sizeof(HakoRecvEventTableType));
     if (datap == nullptr) {
         return false;
     }
-    this->pro_->set_recv_event_table(static_cast<HakoRecvEventTableType*>(datap));
+    this->set_recv_event_table(static_cast<HakoRecvEventTableType*>(datap));
+    return true;
+}
+bool pro::HakoProData::on_pdu_data_reset()
+{
+    HakoRecvEventTableType *table = this->get_recv_event_table();
+    if (table == nullptr) {
+        return false;
+    }
+    table->entry_num = 0;
+    for (int i = 0; i < HAKO_RECV_EVENT_MAX; ++i) {
+        table->entries[i].enabled = false;
+    }
+    return true;
+}
+bool pro::HakoProData::on_pdu_data_destroy()
+{
+    this->destroy();
+    return true;
+}
+
+bool pro::HakoProAssetExtension::on_pdu_data_write(int real_channel_id) 
+{
+    HakoRecvEventTableType *table = this->pro_->get_recv_event_table();
+    if (table == nullptr) {
+        return false;
+    }
+    //process lock is not needed because upper layer is already locked
+    for (int i = 0; i < table->entry_num; ++i) {
+        if (table->entries[i].enabled && (table->entries[i].real_channel_id == real_channel_id)) {
+            table->entries[i].recv_flag = true;
+            break;
+        }
+    }
     return true;
 }
