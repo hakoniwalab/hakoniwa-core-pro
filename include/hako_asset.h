5c22f2f27dfdb2ca7c17e2ad580df169cd8a6ac3
Takashi Mori <kanetugu2015@gmail.com>
2025-04-13 15:54:22 +0900

    add service api

--- include/hako_asset.h
+++ include/hako_asset.h
@@ -1,5 +1,4 @@
-#ifndef _HAKO_ASSET_H_
-#define _HAKO_ASSET_H_
+#pragma once
 
 #include "hako_primitive_types.h"
 
@@ -51,8 +50,96 @@ extern int hako_asset_register_data_recv_event(const char *robo_name, HakoPduCha
  */
 extern int hako_asset_check_data_recv_event(const char *robo_name, HakoPduChannelIdType lchannel);
 
+/**
+ * Create a service server for a specific service name.
+ *
+ * @param serviceName The name of the service.
+ * @param service Callback function to handle the service requests.
+ * @return 0 on success, -1 on failure.
+ */
+extern int hako_service_server_create(const char* serviceName, int (*service)(int));
+
+/**
+ * Get the request packet 
+ * @param service_id The ID of the service.
+ * @param packet The buffer to store the request packet.
+ * @param packet_len The length of the buffer.
+ * @return 0 on success, -1 on failure.
+ */
+extern int hako_service_server_get_request(int service_id, char* packet, size_t packet_len);
+/**
+ * Send a response packet to the client.
+ * @param service_id The ID of the service.
+ * @param packet The response packet.
+ * @param packet_len The length of the response packet.
+ * @return 0 on success, -1 on failure.
+ */
+extern int hako_service_server_put_response(int service_id, char* packet, size_t packet_len);
+
+/**
+ * Check if a service server is running.
+ * @param service_id The ID of the service.
+ * @return 1 if the service server is running, 0 if not, -1 on canceled.
+ */
+extern int hako_service_server_is_canceled(int service_id);
+
+/**
+ * Set the status of the service server.
+ *
+ * @param percentage The percentage of completion (0-100).
+ * @return 0 on success, -1 on failure.
+ */
+extern int hako_service_server_set_status(int percentage);
+
+typedef struct {
+    int service_id;
+    int client_id;
+} HakoServiceHandleType;
+/**
+ * Create a service client for a specific service name.
+ * @param serviceName The name of the service.
+ * @param clientName The name of the client.
+ * @param handle The handle to be used for the service client.
+ * @return 0 on success, -1 on failure.
+ */
+extern int hako_service_client_create(const char* serviceName, const char* clientName, HakoServiceHandleType* handle);
+
+/**
+ * Call a service request.
+ * @param handle The handle of the service client.
+ * @param packet The request packet.
+ * @param packet_len The length of the request packet.
+ * @param timeout The timeout for the request in milliseconds.
+ * @return 0 on success, -1 on failure.
+ */
+extern int hako_service_client_call_request(HakoServiceHandleType* handle, char *packet, size_t packet_len, int timeout);
+
+/**
+ * Get the response packet from the service server.
+ * @param handle The handle of the service client.
+ * @param packet The buffer to store the response packet.
+ * @param packet_len The length of the buffer.
+ * @param timeout The timeout for the response in milliseconds.
+ * @return 0 on success, -1 on failure.
+ */
+extern int hako_service_client_get_response(HakoServiceHandleType* handle, char *packet, size_t packet_len, int timeout);
+
+/**
+ * Cancel the service request.
+ * @param handle The handle of the service client.
+ * @return 0 on success, -1 on failure.
+ */
+extern int hako_service_client_cancel_request(HakoServiceHandleType* handle);
+/**
+ * Check the status of the service client.
+ * @param handle The handle of the service client.
+ * @param status The status of the service client (0: not started, 1: in progress, 2: completed).
+ * @param percentage The percentage of completion (0-100).
+ * @return 0 on success, -1 on failure.
+ */
+extern int hako_service_client_status(HakoServiceHandleType* handle, int* status, int* percentage);
+
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* _HAKO_ASSET_H_ */
