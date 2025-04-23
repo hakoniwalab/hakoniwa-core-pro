5da81fd9f12383832eb84e1fa7c1472c44c2e010
Takashi Mori <kanetugu2015@gmail.com>
2025-04-24 07:57:34 +0900

    refactored sample codes

--- examples/service/src/asset_client.cpp
+++ examples/service/src/asset_client.cpp
@@ -20,6 +20,11 @@ static const char* asset_name = "Client";
 static const char* service_config_path = "./examples/service/service.json";
 
 hako_time_t delta_time_usec = 1000 * 1000;
+static void hako_sleep()
+{
+    hako_asset_usleep(delta_time_usec);
+    usleep(delta_time_usec);
+}
 
 HakoAssetServiceClientTemplateType(AddTwoInts) service_client(asset_name, "Service/Add", "Client01");
 
@@ -40,78 +45,69 @@ static int my_on_manual_timing_control(hako_asset_context_t* context)
     (void)context;
     bool is_timeout_happened = false;
     std::cout << "*************** START SERVICE CLIENT ***************" << std::endl;
+    // 1. request
+    HakoCpp_AddTwoIntsRequest req = {};
+    req.a = 1;
+    req.b = 2;
+    HakoCpp_AddTwoIntsResponse res = {};
+    res.sum = 0;
+    std::cout << "INFO: APL request: a=" << req.a << " b=" << req.b << std::endl;
+    std::cout << "INFO: APL request..." << std::endl;
+    int ret = service_client.request(req, 2000, -1);
+    if (ret < 0) {
+        printf("ERORR: service_client.request() returns %d.\n", ret);
+        return 1;
+    }
+    std::cout << "INFO: APL wait for response " << ret << std::endl;
+    // 2. poll
     while (true) {
-        hako_asset_usleep(delta_time_usec);
-        usleep(delta_time_usec);
-        int ret = service_client.poll();
+        hako_sleep();
+        ret = service_client.poll();
         if (ret < 0) {
-            printf("ERORR: hako_asset_service_client_poll() returns %d.\n", ret);
+            printf("ERORR: service_client.poll() returns %d.\n", ret);
             return 1;
         }
-        std::cout << "INFO: APL STAUTS: " << service_client.status() << std::endl;
-        if (is_timeout_happened) {
-            printf("ERORR: APL request timeout.\n");
-            if (service_client.cancel_request()) {
-                printf("INFO: APL request is canceled.\n");
-                is_timeout_happened = false;
-            }
-            else if (service_client.status() == HAKO_SERVICE_CLIENT_API_STATE_IDLE) {
-                is_timeout_happened = false;
-                printf("INFO: APL success response is received after canceling request....\n");
-                HakoCpp_AddTwoIntsResponse res = service_client.get_response();
-                std::cout << "OUT: sum=" << res.sum << std::endl;
-                while (true) {
-                    hako_asset_usleep(delta_time_usec);
-                    usleep(delta_time_usec);
-                }
-            }
-            else {
-                printf("ERORR: APL request cancel failed.\n");
-                continue;
-            }
-        }
-        if (service_client.status() == HAKO_SERVICE_CLIENT_API_STATE_CANCELING) {
-            continue;
-        }
-        std::cout << "INFO: APL EVENT: " << ret << std::endl;
-        if (ret == HAKO_SERVICE_CLIENT_API_REQUEST_TIMEOUT) {
-            printf("ERORR: APL request timeout.\n");
-            is_timeout_happened = true;
-            continue;
-        }
-        else if (ret == HAKO_SERVICE_CLIENT_API_REQUEST_CANCEL_DONE) {
-            printf("INFO: HAKO_SERVICE_CLIENT_API_REQUEST_CANCEL_DONE\n");
-            while (true) {
-                hako_asset_usleep(delta_time_usec);
-                usleep(delta_time_usec);
-            }
-    }
-        else if ((service_client.status() == HAKO_SERVICE_CLIENT_API_STATE_IDLE) && (ret == HAKO_SERVICE_CLIENT_API_EVENT_NONE))
-        {
-            HakoCpp_AddTwoIntsRequest req = {};
-            req.a = 1;
-            req.b = 2;
-            std::cout << "IN: a=" << req.a << ", b=" << req.b << std::endl;
-            int ret = service_client.request(req, 1000, -1);
-            if (ret < 0) {
-                printf("ERORR: service_client.request() returns %d.\n", ret);
-                return 1;
-            }
-        }
-        else if (ret == HAKO_SERVICE_CLIENT_API_RESPONSE_IN) {
-            HakoCpp_AddTwoIntsResponse res = service_client.get_response();
+        if (service_client.is_response_in(ret)) {
+            res = service_client.get_response();
             std::cout << "OUT: sum=" << res.sum << std::endl;
             break;
         }
-        else if (ret == HAKO_SERVICE_CLIENT_API_EVENT_NONE) {
-            // Do nothing
-        }
-        else {
-            printf("ERORR: hako_asset_service_client_poll() returns %d.\n", ret);
-            return 1;
-
+        if (service_client.is_request_timeout(ret)) {
+            is_timeout_happened = true;
+            break;
         }
     }
+    // 3. if request_timeout, then cancel request
+    if (is_timeout_happened) {
+        printf("WARNING: APL cancel request is happened.\n");
+        while (service_client.cancel_request() == false) {
+            hako_sleep();
+            std::cout << "INFO: APL cancel_request() is not done." << std::endl;
+        }
+        std::cout << "INFO: APL cancel_request() is done." << std::endl;
+        while (true) {
+            ret = service_client.poll();
+            if (service_client.is_request_cancel_done(ret)) {
+                printf("INFO: APL cancel request is done.\n");
+                break;
+            }
+            else if (service_client.is_response_in(ret)) {
+                res = service_client.get_response();
+                std::cout << "OUT: sum=" << res.sum << std::endl;
+                break;
+            }
+            else {
+                printf("ERORR: APL cancel request is not done.\n");
+            }
+            hako_sleep();
+        }
+    }
+    // finish
+    std::cout << "*************** FINISH SERVICE CLIENT ***************" << std::endl;
+    while (true) {
+        hako_sleep();
+    }
+    
     return 0;
 }
 static int my_on_reset(hako_asset_context_t* context)
--- examples/service/src/asset_server.cpp
+++ examples/service/src/asset_server.cpp
@@ -21,7 +21,16 @@ static const char* asset_name = "Server";
 static const char* service_config_path = "./examples/service/service.json";
 static const char* service_name = "Service/Add";
 static hako_time_t delta_time_usec = 1000 * 1000;
-
+static void hako_sleep()
+{
+    hako_asset_usleep(5 * delta_time_usec);
+    usleep(5 * delta_time_usec);
+}
+static void hako_sleep1()
+{
+    hako_asset_usleep(delta_time_usec);
+    usleep(delta_time_usec);
+}
 
 HakoAssetServiceServerTemplateType(AddTwoInts) service_server(asset_name, service_name);
 
@@ -36,49 +45,107 @@ static int my_on_initialize(hako_asset_context_t* context)
     //std::cout << "INFO: service_server.initialize() returns " << ret << std::endl;
     return 0;
 }
-
-static int my_on_manual_timing_control(hako_asset_context_t* context)
+enum class TestCase {
+    NORMAL,
+    CANCEL_1,
+    CANCEL_2,
+};
+static TestCase test_case = TestCase::NORMAL;
+static void cancel_1_test_case()
 {
-    (void)context;
-    std::cout << "*************** START SERVICE SERVER ***************" << std::endl;
+    std::cout << "INFO: wait for while...";
+    hako_sleep();
+    std::cout << "INFO: APL poll..." << std::endl;
+    int ret = service_server.poll();
+    if (ret < 0) {
+        printf("ERORR: hako_asset_service_server_poll() returns %d.\n", ret);
+        return;
+    }
+    if (service_server.is_request_in(ret)) {
+        HakoCpp_AddTwoIntsRequest req = service_server.get_request();
+        HakoCpp_AddTwoIntsResponse res = {};
+        res.sum = req.a + req.b;
+        std::cout << "IN: a=" << req.a << " b=" << req.b << std::endl;
+        hako_sleep();
+        int ret = service_server.poll();
+        std::cout << "INFO: APL EVENT: " << ret << std::endl;
+        if (service_server.is_request_cancel(ret)) {
+            printf("WARNING: APL cancel request is happened.\n");
+            service_server.cancel_reply(res);
+        }
+        else {
+            std::cout << "OUT: sum=" << res.sum << std::endl;
+            (void)service_server.normal_reply(res);
+        }
+    }
+}
+static void cancel_2_test_case()
+{
+    std::cout << "INFO: wait for while...";
+    hako_sleep();
+    std::cout << "INFO: APL poll..." << std::endl;
+    int ret = service_server.poll();
+    if (ret < 0) {
+        printf("ERORR: hako_asset_service_server_poll() returns %d.\n", ret);
+        return;
+    }
+    if (service_server.is_request_in(ret)) {
+        HakoCpp_AddTwoIntsRequest req = service_server.get_request();
+        HakoCpp_AddTwoIntsResponse res = {};
+        res.sum = req.a + req.b;
+        std::cout << "IN: a=" << req.a << " b=" << req.b << std::endl;
+        hako_sleep();
+        std::cout << "OUT: sum=" << res.sum << std::endl;
+        (void)service_server.normal_reply(res);
+        int ret = service_server.poll();
+        std::cout << "INFO: APL EVENT: " << ret << std::endl;
+    }
+}
+static void normal_test_case()
+{
+    std::cout << "INFO: APL poll..." << std::endl;
     while (true) {
-        hako_asset_usleep(5* delta_time_usec);
-        usleep(5* delta_time_usec);
-
         int ret = service_server.poll();
         if (ret < 0) {
             printf("ERORR: hako_asset_service_server_poll() returns %d.\n", ret);
-            return 1;
+            return;
         }
         if (service_server.is_request_in(ret)) {
             HakoCpp_AddTwoIntsRequest req = service_server.get_request();
             HakoCpp_AddTwoIntsResponse res = {};
             res.sum = req.a + req.b;
             std::cout << "IN: a=" << req.a << " b=" << req.b << std::endl;
-#if false /* 2. for racing condition: cancel request is received after reply... */
-            hako_asset_usleep(5* delta_time_usec);//cancel request is sended while sleeping..
-            usleep(5* delta_time_usec);
             std::cout << "OUT: sum=" << res.sum << std::endl;
-            (void)service_server.normal_reply(res);
-            int ret = service_server.poll();
-            std::cout << "INFO: APL EVENT: " << ret << std::endl;
-#else      /* 1. for racing condition: cancel request is received before reply... */
-            hako_asset_usleep(5* delta_time_usec);
-            usleep(5* delta_time_usec);
-            int ret = service_server.poll();
-            std::cout << "INFO: APL EVENT: " << ret << std::endl;
-            if (service_server.is_request_cancel(ret)) {
-                printf("WARNING: APL cancel request is happened.\n");
-                service_server.cancel_reply(res);
+            while (service_server.normal_reply(res) == false) {
+                hako_sleep1();
+                std::cout << "INFO: APL normal_reply() is not done." << std::endl;
             }
-            else {
-                std::cout << "OUT: sum=" << res.sum << std::endl;
-                (void)service_server.normal_reply(res);
-            }
-#endif
+            break;
         }
-        hako_asset_usleep(delta_time_usec);
-        usleep(delta_time_usec);
+        else {
+            printf("ERORR: APL is not request_in.\n");
+        }
+        hako_sleep1();
+    }
+}
+
+static int my_on_manual_timing_control(hako_asset_context_t* context)
+{
+    (void)context;
+    std::cout << "*************** START SERVICE SERVER ***************" << std::endl;
+    switch (test_case) {
+        case TestCase::NORMAL:
+            normal_test_case();
+            break;
+        case TestCase::CANCEL_1:
+            cancel_1_test_case();
+            break;
+        case TestCase::CANCEL_2:
+            cancel_2_test_case();
+            break;
+    }
+    while (true) {
+        hako_sleep();
     }
     return 0;
 }
@@ -97,12 +164,23 @@ static hako_asset_callbacks_t my_callback = {
 };
 int main(int argc, const char* argv[])
 {
-    if (argc != 2) {
-        printf("Usage: %s <config_path>\n", argv[0]);
+    if (argc < 3) {
+        printf("Usage: %s <config_path> <test_case: normal | cancel1 | cancel2>\n", argv[0]);
         return 1;
     }
     const char* config_path = argv[1];
+    const char* case_name = argv[2];
 
+    if (strcmp(case_name, "normal") == 0) {
+        test_case = TestCase::NORMAL;
+    } else if (strcmp(case_name, "cancel1") == 0) {
+        test_case = TestCase::CANCEL_1;
+    } else if (strcmp(case_name, "cancel2") == 0) {
+        test_case = TestCase::CANCEL_2;
+    } else {
+        printf("ERROR: Unknown test case: %s\n", case_name);
+        return 1;
+    }
     hako_conductor_start(delta_time_usec, delta_time_usec);
     int ret = hako_asset_register(asset_name, config_path, &my_callback, delta_time_usec, HAKO_ASSET_MODEL_PLANT);
     if (ret != 0) {
