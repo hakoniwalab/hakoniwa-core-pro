72136ede02869587c527987884dff23b15bb21e5
tmori <kanetugu2015@gmail.com>
2025-08-11 16:26:35 +0900

    moved topic to python repo

--- examples/external/topic/publisher.py
+++ examples/external/topic/publisher.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 import time
 import asyncio
-from sources.assets.bindings.python.shm_common import ShmCommon
-from sources.assets.bindings.python.shm_topic_publisher import ShmPublisher
-
+from hakoniwa_pdu.service.shm_common import ShmCommon
+from hakoniwa_pdu.topic.shm_topic_publisher import ShmPublisher
+from hakoniwa_pdu.pdu_msgs.geometry_msgs.pdu_pytype_Twist import Twist
+from hakoniwa_pdu.pdu_msgs.geometry_msgs.pdu_conv_Twist import pdu_to_py_Twist, py_to_pdu_Twist
 
 async def main_async():
     service_config_path = "examples/external/topic/service.json"
@@ -13,17 +14,20 @@ async def main_async():
     shm = ShmCommon(service_config_path, pdu_offset_path, delta_time_usec)
     shm.initialize()
 
-    publisher = ShmPublisher(shm, "drone2", "pos")
+    publisher = ShmPublisher(shm, "drone2", "pos", py_to_pdu_Twist)
     publisher.initialize(service_config_path)
 
-    msg = publisher.get_empty_message()
+    msg : Twist = Twist()
     count = 0
 
     while True:
-        msg['linear']['x'] = count
-        msg['angular']['z'] = count * 0.1
-        publisher.publish(msg)
-        print(f"Published: {msg}")
+        msg.linear.x = count
+        msg.angular.z = count * 0.1
+        ret = publisher.publish(msg)
+        if ret:
+            print(f"Success Published: {msg}")
+        else:
+            print("Failed to publish message")
         count += 1
         await shm.sleep()
 
--- examples/external/topic/subscriber.py
+++ examples/external/topic/subscriber.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 import time
 import asyncio
-from sources.assets.bindings.python.shm_common import ShmCommon
-from sources.assets.bindings.python.shm_topic_subscriber import ShmSubscriber
+from hakoniwa_pdu.service.shm_common import ShmCommon
+from hakoniwa_pdu.topic.shm_topic_subscriber import ShmSubscriber
+from hakoniwa_pdu.pdu_msgs.geometry_msgs.pdu_pytype_Twist import Twist
+from hakoniwa_pdu.pdu_msgs.geometry_msgs.pdu_conv_Twist import pdu_to_py_Twist, py_to_pdu_Twist
 
-def on_pos_received(msg):
+def on_pos_received(msg: Twist):
     print(f"Received in callback: {msg}")
 
 async def main_async():
@@ -17,7 +19,7 @@ async def main_async():
     shm.initialize()
 
     shm.start_service()
-    subscriber = ShmSubscriber(shm, "drone2", "pos")
+    subscriber = ShmSubscriber(shm, "drone2", "pos", pdu_to_py_Twist)
     subscriber.initialize(service_config_path, on_pos_received)
 
     await subscriber.spin()
