910227ec953696c20a7f56b4ce2c302626cec2e5
Takashi Mori <kanetugu2015@gmail.com>
2026-01-18 12:00:04 +0900

    fix #40

--- sources/conductor/src/hako_conductor.cpp
+++ sources/conductor/src/hako_conductor.cpp
@@ -3,7 +3,12 @@
 
 int hako_conductor_start(hako_time_t delta_usec, hako_time_t max_delay_usec)
 {
-    return hako_conductor_impl_start(delta_usec, max_delay_usec);
+    if (hako_conductor_impl_start(delta_usec, max_delay_usec)) {
+        return 0;
+    }
+    else {
+        return -1;
+    }
 }
 
 void hako_conductor_stop(void)
