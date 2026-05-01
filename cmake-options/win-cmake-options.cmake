e62f51feedebc248942cc260a49b2f16f496739a
tmori <kanetugu2015@gmail.com>
2026-05-01 13:25:04 +0900

    compilation error is fixed from mujoco win

--- cmake-options/win-cmake-options.cmake
+++ cmake-options/win-cmake-options.cmake
@@ -2,12 +2,14 @@ set(CMAKE_CXX_STANDARD 20)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 add_compile_options(/std:c++20 /Zc:__cplusplus)
 
+if (MSVC)
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}")
+else()
 set(CMAKE_C_FLAGS "-std=gnu99")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall")
-
-set(CMAKE_CXX_STANDARD 17)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall")
+endif()
 
 add_compile_definitions(HAKO_CORE_EXTENSION=TRUE)
 add_compile_definitions(FIX_PDU_CREATE_TIMING=TRUE)
